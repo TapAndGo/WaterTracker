6eee06a28d9b16611e42de7b4249f4450ca1d67b
ordo-chao <princeben9312@gmail.com>
2025-12-01 18:14:49 +0300

    Fix: Test

--- Utils/db.js
+++ Utils/db.js
@@ -1,19 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-// Hardcoded environment variables
-const POSTGRES_DB = 'watertracker';
-const POSTGRES_USER = 'postgres';
-const POSTGRES_PASSWORD = 'Pr!nce9312lee';
-const POSTGRES_HOST = 'watertracker-postgres-service';
-const POSTGRES_PORT = 5432;
-
 const sequelize = new Sequelize(
-  POSTGRES_DB,
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
   {
-    host: POSTGRES_HOST,
-    port: POSTGRES_PORT,
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT, 10),
     dialect: 'postgres',
     logging: false,
   }
