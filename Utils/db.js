be15cf62356047307efb5cbd32dbb1b8ceff17f0
ordo-chao <princeben9312@gmail.com>
2025-12-01 17:39:32 +0300

    Fix: added dontenv override to false in db.js

--- Utils/db.js
+++ Utils/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({override: false});
 
 const sequelize = new Sequelize(
   process.env.POSTGRES_DB,      // db name
