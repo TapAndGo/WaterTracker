a99336917a1dae1f49f85da601c9576232867cd3
ordo-chao <princeben9312@gmail.com>
2025-12-01 17:10:04 +0300

    Fix: change from db to postgres

--- Utils/db.js
+++ Utils/db.js
@@ -1,13 +1,18 @@
-// db.js
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,      // db name
+  process.env.POSTGRES_USER,    // user
+  process.env.POSTGRES_PASSWORD, // password
+  {
+    host: process.env.POSTGRES_HOST, // host
+    port: process.env.POSTGRES_PORT, // port
+    dialect: 'postgres',
+    logging: false,
+  }
+);
 
 export default sequelize;
