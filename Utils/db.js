954aa452e175e246e5d6c7b625471406deaf7315
ordo-chao <princeben9312@gmail.com>
2025-12-01 17:51:34 +0300

    Fix: added dontenv override to false in db.js

--- Utils/db.js
+++ Utils/db.js
@@ -1,15 +1,15 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-dotenv.config({override: false});
+dotenv.config({ override: true }); // always use K8s env
 
 const sequelize = new Sequelize(
-  process.env.POSTGRES_DB,      // db name
-  process.env.POSTGRES_USER,    // user
-  process.env.POSTGRES_PASSWORD, // password
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
   {
-    host: process.env.POSTGRES_HOST, // host
-    port: process.env.POSTGRES_PORT, // port
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT, 10),
     dialect: 'postgres',
     logging: false,
   }
